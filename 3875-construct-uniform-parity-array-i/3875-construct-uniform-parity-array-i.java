c681a4968f3f1e230f8effa898d2f8e5d2aaddf5
Ankitdey2123 <164650880+Ankitdey2123@users.noreply.github.com>
2026-09-02 10:00:00 +0530

    Time: 1 ms (31.89%), Space: 45.5 MB (5.31%) - LeetHub

--- 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.java
+++ 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.java
@@ -1,15 +1,5 @@
 class Solution {
     public boolean uniformArray(int[] nums1) {
-        for(int i=0;i<nums1.length;i++){
-            if(i>=nums1.length-1){
-                if(nums1[i]%2==0 || nums1[i]%2!=0){
-                    return true;
-                }
-            }
-            if(nums1[i]-nums1[i+1]%2==0 || nums1[i]-nums1[i+1]%2==0){
-                return true;
-            }
-        }
-            return false;
+        return true;
     }
 }
