e6a3d27e35d63a669132b668379fd305b0246ca6
Ankitdey2123 <164650880+Ankitdey2123@users.noreply.github.com>
2026-09-04 16:55:18 +0530

    Time: 1 ms (99.79%), Space: 46.2 MB (70.85%) - LeetHub

---
+++ 3903-smallest-stable-index-i/3903-smallest-stable-index-i.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int firstStableIndex(int[] nums, int k) {
+        int[] pre = new int[nums.length];
+        int[] suf = new int[nums.length];
+
+        pre[0] = nums[0];
+        suf[nums.length - 1] = nums[nums.length - 1];
+
+        // Prefix maximum
+        for (int i = 1; i < nums.length; i++) {
+            pre[i] = Math.max(pre[i - 1], nums[i]);
+        }
+
+        // Suffix minimum
+        for (int i = nums.length - 2; i >= 0; i--) {
+            suf[i] = Math.min(suf[i + 1], nums[i]);
+        }
+
+        // Find first stable index
+        for (int i = 0; i < nums.length; i++) {
+            if (pre[i] - suf[i] <= k) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+}
